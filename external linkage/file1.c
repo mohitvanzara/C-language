2fbb47373f5bb0a741fee15e19febf67b32c597f
mohit vanzara <mohitvanzara32@gmail.com>
2026-07-16 23:42:09 +0530

    understand linkage topic using example

---
+++ external linkage/file1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int count =10; //external linkage
+
+void display(){
+    printf("Count is %d\n", count);
+}
---
+++ external linkage/file2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+extern int count ;
+extern void display();
+
+int main(){
+    display();
+    return 0;
+}
